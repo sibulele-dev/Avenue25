9edbfad4956fd0ebd1a342109c11c4b0e59ff6d9
sibulele-dev <mduzulwanaesethu@gmail.com>
2022-08-05 14:12:30 +0200

    DB connection

---
+++ Models/Clients.js
@@ -0,0 +1,23 @@
+const mongoose= require('mongoose');
+
+const ClientSchema = new mongoose.schema({
+    Name: {
+        type: String,
+        required: [true, ' Please Enter Your Name']
+    },
+    Phone:{
+        type: Number,
+        required: [true, 'Please enter phone number'],
+        maxLength:[10, 'Phone number cannot be more than 10 characters'],
+
+    },
+    Email: {
+        type: String,
+        required: [true, 'Please enter your email address'],
+    },
+    Message: {
+        type: String,
+        required: [true, 'Please Enter your message so we can know what to help you with']
+    }
+})
+module.exports = mongoose.model.Client || mongoose.model('Client', ClientSchema );
