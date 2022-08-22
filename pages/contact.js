1ef04aa2a760ae8739fca9c2923d306404673913
sibulele-dev <mduzulwanaesethu@gmail.com>
2022-08-22 18:31:53 +0200

    contact form

--- pages/contact.js
+++ pages/contact.js
@@ -1,94 +1,88 @@
 import Head from "next/head";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../components/Layout/Layout";
 import Banner from "../components/Banner/Banner";
-import axios from "axios";
-import { useRouter } from "next/router";
+// import axios from "axios";
+// import { useRouter } from "next/router";
 
 export default function contact() {
-
   // const [name, setName] = useState("");
   // const [tel, setTel] =useState("");
   // const [email, setEmail] =useState("");
   // const [message, setMessage] =useState("");
 
-  const [form, setForm]= useState({fullname: '',tel: '',email: '',message: ''});
+  // const [form, setForm]= useState({fullname: '',tel: '',email: '',message: ''});
 
-  const [submitting, setSubmitting] = useState(false);
-  const router = useRouter()
-  const [errors, setErrors] =useState({})
+  // const [submitting, setSubmitting] = useState(false);
+  // const router = useRouter()
+  // const [errors, setErrors] =useState({})
 
+  // useEffect( () => {
+  //   if(submitting){
+  //     if(Object.keys(errors).length === 0){
+  //       createClient()
+  //     }
+  //     else {
+  //       setSubmitting(false)
+  //     }
+  //   }
+  // }, [errors])
 
-  useEffect( () => {
-    if(submitting){
-      if(Object.keys(errors).length === 0){
-        createClient()
-      }
-      else {
-        setSubmitting(false)
-      }
-    }
-  }, [errors])
+  // const createClient = async() =>{
+  //   try {
+  //       const res = await fetch('https//localhost:3000/api/Client', {
+  //         method: 'POST',
+  //         headers: {
+  //           "Accept": "application/json",
+  //           "Content-Type": "application/json"
+  //         },
+  //         body: JSON.stringify(form)
+  //       });
+  //       router.push('/')
 
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
 
-  const createClient = async() =>{
-    try {
-        const res = await fetch('https//localhost:3000/api/Client', {
-          method: 'POST',
-          headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-          },  
-          body: JSON.stringify(form)
-        });
-        router.push('/')
-      
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  //   let errs =validate();
+  //   setErrors(errs)
+  //   setSubmitting(true)
+  // }
 
+  // const handleChange = (e) => {
+  //   setForm({
+  //     ...form,//spreads out current form state
 
-    let errs =validate();
-    setErrors(errs)
-    setSubmitting(true)
-  }
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
 
-  const handleChange = (e) => {
-    setForm({
-      ...form,//spreads out current form state
+  // const validate = () => {
+  //   let err = {}
 
-      [e.target.name]: e.target.value
-    })
-  }
+  //   if(!form.fullname) {
+  //     err.fullname ="Please enter your Name "
+  //   }
 
-  const validate = () => {
-    let err = {}
+  //   if(!form.email) {
+  //     err.email ="Please enter your email "
+  //   }
 
+  //   if(!form.tel) {
+  //     err.tel ="Please enter your Phone number "
+  //   }
 
-    if(!form.fullname) {
-      err.fullname ="Please enter your Name "
-    }
+  //   if(!form.message) {
+  //     err.message ="Please enter your Message, Query or Text "
+  //   }
 
-    if(!form.email) {
-      err.email ="Please enter your email "
-    }
-
-    if(!form.tel) {
-      err.tel ="Please enter your Phone number "
-    }
-
-    if(!form.message) {
-      err.message ="Please enter your Message, Query or Text "
-    }
-
-    return err;
-  }
+  //   return err;
+  // }
   return (
-    <div className="">
-      
+    <div>
       <Head>
         <title>Avenue25 Contact</title>
         <meta content="Contact page with conact for of the official Avenue 25 page " />
@@ -103,59 +97,93 @@ export default function contact() {
           </div>
         </Banner>
         <div>
-        <div className="text-center w-full">
-
-      </div>
-      <div
-        className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
-        <div className="flex flex-col justify-between">
-          <div>
-            <h2 className="text-4xl lg:text-5xl font-bold leading-tight">Lets talk about everything!</h2>
-            <div className="text-gray-700 mt-8">
-              Hate forms? Send us an <a className="underline cursor-pointer text-blue-600" href="mailto:info@avenue25.co.za" >email</a> instead.
+          <div className="text-center w-full"></div>
+          <div className="max-w-screen-xl mt-24 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
+            <div className="flex flex-col justify-between">
+              <div>
+                <h2 className="text-4xl lg:text-5xl font-bold leading-tight">
+                  Lets talk about everything!
+                </h2>
+                <div className="text-gray-700 mt-8">
+                  Hate forms? Send us an{" "}
+                  <a
+                    className="underline cursor-pointer text-blue-600"
+                    href="mailto:info@avenue25.co.za"
+                  >
+                    email
+                  </a>{" "}
+                  instead.
+                </div>
+              </div>
+              <div className="mt-8 text-center">
+                <img
+                  className="hidden md:block h-[28rem]"
+                  src="https://ik.imagekit.io/knqnox49e/options_dOGT6nF1Z.png?ik-sdk-version=javascript-1.4.3&updatedAt=1661166859877"
+                  alt="Lady with options"
+                />
+              </div>
             </div>
-          </div>
-          <div className="mt-8 text-center">
-            <img className="hidden md:block h-[28rem]" src="https://ik.imagekit.io/knqnox49e/options_dOGT6nF1Z.png?ik-sdk-version=javascript-1.4.3&updatedAt=1661166859877" alt="Lady with options" />
-          </div>
-        </div>
 
-        {/* you can use isSubmitting to put a loader when submitting and a form whe not submitting */}
-        <form className="" onSubmit={handleSubmit} method="POST">
-          <div>
-            <span className="uppercase text-sm text-gray-600 font-bold">Full Name</span>
-            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
-              type="text" placeholder="Jane Doe" name="fullname"  onChange={handleChange} />
+            {/* you can use isSubmitting to put a loader when submitting and a form whe not submitting */}
+            <form className="" method="POST">
+              <div>
+                <span className="uppercase text-sm text-gray-600 font-bold">
+                  Full Name
+                </span>
+                <input
+                  className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  type="text"
+                  placeholder="Jane Doe"
+                  name="fullname"
+                />
+              </div>
+              <div className="mt-8">
+                <span className="uppercase text-sm text-gray-600 font-bold">
+                  Email
+                </span>
+                <input
+                  className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  placeholder="janedoe@domain.com"
+                  type="email"
+                  name="email"
+                />
+              </div>
+              <div className="mt-8">
+                <span className="uppercase text-sm text-gray-600 font-bold">
+                  Phone
+                </span>
+                <input
+                  className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  placeholder="+27 (12) 345 6789"
+                  type="tel"
+                  name="tel"
+                />
+              </div>
+              <div className="mt-8">
+                <span className="uppercase text-sm text-gray-600 font-bold">
+                  Message
+                </span>
+                <textarea
+                  className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                  name="message"
+                ></textarea>
+              </div>
+              <div className="mt-8">
+                <button
+                  className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Send Message
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="mt-8">
-            <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
-            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" placeholder="janedoe@domain.com"
-              type="email"  name="email" onChange={handleChange} />
-          </div>
-          <div className="mt-8">
-            <span className="uppercase text-sm text-gray-600 font-bold">Phone</span>
-            <input className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" placeholder="+27 (12) 345 6789"
-              type="tel"  name="tel" onChange={handleChange} />
-          </div>
-          <div className="mt-8">
-            <span className="uppercase text-sm text-gray-600 font-bold">Message</span>
-            <textarea
-              className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" name="message"  onChange={handleChange}></textarea>
-          </div>
-          <div className="mt-8">
-            <button
-              className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline" type="submit">
-              Send Message
-            </button>
-          </div>
-        </form>
-      </div>
         </div>
         <br />
-          <br />
-          <br />
-          <br />
-          <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </Layout>
     </div>
   );
